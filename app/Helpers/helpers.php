a83df45ce92373914d1db6162f115b0899f8d9bd
asrani <asrani.27@gmail.com>
2021-01-02 16:21:47 +0800

    fix

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Pasien;
+use Carbon\Carbon;
+
+function sekarang()
+{
+    return Carbon::now()->format('Y-m-d');
+}
 
 function konfirmasi()
 {
@@ -22,18 +28,18 @@ function probable()
 
 function konfirmasiToday()
 {
-    $data = count(Pasien::where('hasil','KONFIRMASI')->get());
+    $data = count(Pasien::where('hasil','KONFIRMASI')->where('created_at', 'LIKE', '%'.sekarang().'%')->get());
     return $data;
 }
 
 function suspectToday()
 {
-    $data = count(Pasien::where('hasil','SUSPECT')->get());
+    $data = count(Pasien::where('hasil','SUSPECT')->where('created_at', 'LIKE', '%'.sekarang().'%')->get());
     return $data;
 }
 
 function probableToday()
 {
-    $data = count(Pasien::where('hasil','PROBABLE')->get());
+    $data = count(Pasien::where('hasil','PROBABLE')->where('created_at', 'LIKE', '%'.sekarang().'%')->get());
     return $data;
 }
